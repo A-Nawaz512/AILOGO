7d34bfc488dd52cc213cbcfe2aa77880e8cbdd51
TalhaCh-Coder <talhaahmeduni022@gmail.com>
2025-12-01 14:41:36 +0500

    About page is created but still under working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Navbar from "./components/commonComponents/CoustomerNavbar";
-import HomePage from "../pages/HomePage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 const MainLayout = () => (
    <div className="lg:mx-auto lg:max-w-[1536px]">
@@ -15,7 +16,10 @@ const MainLayout = () => (
 const router = createBrowserRouter([
    {
       element: <MainLayout />,
-      children: [{ path: "/", element: <HomePage /> }],
+      children: [
+         { path: "/", element: <HomePage /> },
+         { path: "/about", element: <AboutPage /> },
+      ],
    },
    // { path: "*", element: <PageNotFound /> },
 ]);
